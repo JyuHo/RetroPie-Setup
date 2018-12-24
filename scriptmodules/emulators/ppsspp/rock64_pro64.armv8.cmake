38d1b8b3eb0bcfc16314cad59b3f940aa21f68cb
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-12-24 12:25:03 +0100

    Update rock64_pro64.armv8.cmake

--- scriptmodules/emulators/ppsspp/rock64_pro64.armv8.cmake
+++ scriptmodules/emulators/ppsspp/rock64_pro64.armv8.cmake
@@ -1,12 +1,18 @@
 include_directories(SYSTEM
-  /usr/local/include
   /usr/include
+  /usr/include/GLES2
+  /usr/include/EGL
 )
 
 set(ARCH_FLAGS "-O2 -marm -march=armv8-a+crc -mtune=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations -pipe")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARCH_FLAGS}"  CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARCH_FLAGS}" CACHE STRING "" FORCE)
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${ARCH_FLAGS}" CACHE STRING "" FORCE)
+
+set(CMAKE_EXE_LINKER_FLAGS "-L/usr/lib/arm-linux-gnueabihf/" CACHE STRING "" FORCE)
+
+set(OPENGL_LIBRARIES /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2)
+set(EGL_LIBRARIES /usr/lib/arm-linux-gnueabihf/libEGL.so)
 
 set(OPENGL_LIBRARIES GLESv2)
 set(ARMV7 ON)
